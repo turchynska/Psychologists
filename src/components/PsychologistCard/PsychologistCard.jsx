79bad912ae3b4ececd8e84af31095c39e93935bb
Kate <159571202+turchynska@users.noreply.github.com>
2025-03-19 21:49:05 -0400

    Psycholodist card
    need to add styles

--- src/components/PsychologistCard/PsychologistCard.jsx
+++ src/components/PsychologistCard/PsychologistCard.jsx
@@ -27,13 +27,42 @@ const PsychologistCard = ({ psychologist }) => {
         <div className={css.cardInfo}>
           <span className={css.rating}>
             <AiFillStar className={css.starIcon} /> {rating}
+            <span className={css.reviews}>({reviews} reviews)</span>
           </span>
           <span className={css.divider}>|</span>
           <span className={css.price}>
             Price / 1 hour:{" "}
-            <span className={css.highlight-price}>${price_per_hour}</span>
+            <span className={css.highlightPrice}>${price_per_hour}</span>
           </span>
-                <AiOutlineHeart className={css.heart - icon} />
+          <AiOutlineHeart className={css.heartIcon} />
+        </div>
+        <div className={css.cardTags}>
+          <span className={css.tag}>
+            Experience: <strong>{experience} years</strong>
+          </span>
+          <span className={css.tag}>License: {license}</span>
+        </div>
+        <div className={css.specialization}>
+          <span>
+            Specialization:{" "}
+            <a href="#specialization" className={css.link}>
+              {specialization}
+            </a>
+          </span>
+          <span>
+            | Initial consultation:{" "}
+            <a href="#consultation" className={css.link}>
+              {initial_consultation
+                ? "Free 45-minute initial consultation"
+                : "Not available"}
+            </a>
+          </span>
+        </div>
+        <p className={css.about}>{about}</p>
+        <div>
+          <a href="#read-more" className={css.readMore}>
+            Read more
+          </a>
         </div>
       </>
     );
